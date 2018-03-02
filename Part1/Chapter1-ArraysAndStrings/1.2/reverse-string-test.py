8c2d7fc0a4c3333ea29473753a3aeb2d1fe9f14d
madhurya <madhurya.dgp@gmail.com>
2018-03-02 10:24:25 -0500

    adds edge tests

--- Part1/Chapter1-ArraysAndStrings/1.2/reverse-string-test.py
+++ Part1/Chapter1-ArraysAndStrings/1.2/reverse-string-test.py
@@ -10,5 +10,11 @@ class UniqueCharTest(unittest.TestCase):
     def test_sentences(self):
         self.assertEqual(reverse_string("The animals are drinking water"),"retaw gniknird era slamina ehT")
 
+    def test_empty_string(self):
+        self.assertEqual(reverse_string(" "), " ")
+
+    def test_no_string(self):
+        self.assertEqual(reverse_string(""), "")
+
 if __name__ == '__main__':
     unittest.main()
